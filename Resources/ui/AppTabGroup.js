1ad891e08c570b0f2f737c3b1d9896a280347d9a
Y.Nishimura <nishimuray@gmail.com>
2012-04-12 02:37:27 +0900

    タブを６つに。このおかげで右端のタブがその他になって、余ってる部分をリスト表示、右肩のボタンでダッシュボードビュー、タブの入れ替え表示、コントロールの変更が可能であることを確認（iOS）

--- Resources/ui/AppTabGroup.js
+++ Resources/ui/AppTabGroup.js
@@ -10,6 +10,8 @@ function AppTabGroup() {
 		win_setting = new AppWindow(L('settings'))
 		win_list    = new AppWindow(L('list'))
 		win_image   = new AppWindow(L('image'))
+		win_scroll  = new AppWindow(L('scroll'))
+		win_form    = new AppWindow(L('form'))
 		;
 	
 	var tab_home = Ti.UI.createTab({
@@ -40,10 +42,26 @@ function AppTabGroup() {
 	});
 	win_image.containingTab = tab_image;
 	
+	var tab_scroll = Ti.UI.createTab({
+		title: L('scroll'),
+		icon: '/images/KS_nav_ui.png',
+		window: win_scroll
+	});
+	win_scroll.containingTab = tab_scroll;
+	
+	var tab_form = Ti.UI.createTab({
+		title: L('form'),
+		icon: '/images/KS_nav_views.png',
+		window: win_form
+	});
+	win_form.containingTab = tab_form;
+	
 	self.addTab(tab_home);
 	self.addTab(tab_setting);
 	self.addTab(tab_list);
 	self.addTab(tab_image);
+	self.addTab(tab_scroll);
+	self.addTab(tab_form);
 	
 	return self;
 };
